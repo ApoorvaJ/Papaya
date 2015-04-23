f0aafa19573bad35357d89340bfa5ca749bcb624
Apoorva Joshi <apoorva.ramesh.joshi@gmail.com>
2015-04-23 15:07:31 +0530

    Temporarily switched to VS 2012, Added basic stb_image integration

--- src/papaya.cpp
+++ src/papaya.cpp
@@ -1,12 +1,48 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+global_variable uint8 *Image;
 
 internal void RenderWeirdGradient(
 	game_offscreen_buffer *Buffer, 
 	int BlueOffset, 
 	int GreenOffset)
 {
+	local_persist int ImageWidth, ImageHeight, ComponentsPerPixel;
+	if (!Image)
+	{
+		Image =	stbi_load("C:\\Users\\Apoorva\\Pictures\\Camera Roll\\picture000.jpg", &ImageWidth, &ImageHeight, &ComponentsPerPixel, 0);
+	}
+	else
+	{
+		uint8 *Row = (uint8 *)Buffer->Memory;
+		uint8 *ImageCursor = Image;
+
+		for (int Y = 0;
+			Y < Buffer->Height;
+			++Y)
+		{
+			uint32 *Pixel = (uint32 *)Row;
+			for (int X = 0;
+				X < Buffer->Width;
+				++X)
+			{
+				uint8 Red = *ImageCursor++;
+				uint8 Blue =  *ImageCursor++ + BlueOffset;
+				uint8 Green = *ImageCursor++ + GreenOffset;
+
+
+
+				*Pixel++ = ( (Red << 16) | (Green << 8) | Blue);
+			}
+
+			Row += Buffer->Pitch;
+		}
+	}
+
 	// TODO: Let's see what the optimizer does
 
-	uint8 *Row = (uint8 *)Buffer->Memory;
+	/*uint8 *Row = (uint8 *)Buffer->Memory;
 	for (int Y = 0;
 		Y < Buffer->Height;
 		++Y)
@@ -19,11 +55,11 @@ internal void RenderWeirdGradient(
 			uint8 Blue = (X + BlueOffset);
 			uint8 Green = (Y + GreenOffset);
 
-			*Pixel++ = ((Green << 8) | Blue);
+			*Pixel++ = ((Green << 16) | Blue);
 		}
 
 		Row += Buffer->Pitch;
-	}
+	}*/
 }
 
 void GameUpdateAndRender(
