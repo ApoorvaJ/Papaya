fb3942192f72181c37b6f60383a0b67fdd9b5199
Apoorva Joshi <apoorva.ramesh.joshi@gmail.com>
2015-12-21 02:03:17 +0530

    Updated EasyTab

--- src/easytab.h
+++ src/easytab.h
@@ -853,8 +853,8 @@ EasyTabResult EasyTab_HandleEvent(HWND Window, UINT Message, LPARAM LParam, WPAR
 
 void EasyTab_Unload()
 {
-    EasyTab->WTClose(EasyTab->Context);
-    if (EasyTab->Dll) { FreeLibrary(EasyTab->Dll); }
+    if (EasyTab->Context) { EasyTab->WTClose(EasyTab->Context); }
+    if (EasyTab->Dll)     { FreeLibrary(EasyTab->Dll); }
     free(EasyTab);
     EasyTab = NULL;
 }
